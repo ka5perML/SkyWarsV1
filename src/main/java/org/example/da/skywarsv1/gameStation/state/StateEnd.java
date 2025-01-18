9c9b9c17c5e101d89b5401b9ae8c952e2294e62c
ka5perML <ilzatschakirov@gmail.com>
2025-01-18 14:56:05 +0300

    update

--- src/main/java/org/example/da/skywarsv1/gameStation/state/StateEnd.java
+++ src/main/java/org/example/da/skywarsv1/gameStation/state/StateEnd.java
@@ -5,15 +5,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.example.da.skywarsv1.mapSetting.BlockChange;
 import org.example.da.skywarsv1.playerSetting.BlockBreakerForPlayer;
+import org.example.da.skywarsv1.playerSetting.PlayerKill;
 import org.example.da.skywarsv1.playerSetting.PlayerKillCounter;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StateEnd {
-    private BlockChange blockChange;
-    private BlockBreakerForPlayer breaker;
-    private PlayerKillCounter playerKillCounter;
+    private final BlockChange blockChange;
+    private final BlockBreakerForPlayer breaker;
+    private final PlayerKillCounter playerKillCounter;
     public StateEnd(BlockChange blockChange, BlockBreakerForPlayer breaker, PlayerKillCounter playerKillCounter){
         this.blockChange = blockChange;
         this.breaker = breaker;
@@ -25,9 +26,7 @@ public class StateEnd {
     }
     @SneakyThrows
     private void gameEnd(){
-        breaker.getPlayerPutBlock().forEach(location->{
-            blockChange.replaceBlockInAir(location);
-        });
+        breaker.getPlayerPutBlock().forEach(blockChange::replaceBlockInAir);
         Thread.sleep(10000);
         Bukkit.reload();
     }
@@ -37,6 +36,11 @@ public class StateEnd {
                 .filter(player -> player.getGameMode() == GameMode.SURVIVAL)
                 .map(player -> player.getDisplayName())
                 .collect(Collectors.toList());
+        List<PlayerKill> playerKillList = playerKillCounter.getSortedList();
         Bukkit.broadcastMessage("Победитель " + playerList);
+        Bukkit.broadcastMessage("Топ 3 по убийствам \n" +
+                "1. " + playerKillList.get(0).getPlayer() + " = " + playerKillList.get(0).getKillCount() + "\n" +
+                "2. " + playerKillList.get(1).getPlayer() + " = " + playerKillList.get(0).getKillCount() + "\n" +
+                "3. " + playerKillList.get(2).getPlayer() + " = " + playerKillList.get(0).getKillCount());
     }
 }
